65e5a9828b03654f86782739e86157be3c62327c
Allen Jamison <119152205+Teqnixx@users.noreply.github.com>
2024-08-07 14:35:01 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    connectionString: process.env.POSTGRES_URL,
 })
 
 pool.connect((err) => {
